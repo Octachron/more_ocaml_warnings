8801a52270fb990a1f37c1ec016402ed8da12712
octachron <octa@polychoron.fr>
2018-09-29 12:24:12 +0200

    Boolean formula for edges

--- opaqueness/hypergraph.ml
+++ opaqueness/hypergraph.ml
@@ -1,6 +1,62 @@
+let debug fmt = Format.fprintf Format.err_formatter
+    ("@[debug:@ " ^^ fmt ^^ "@]@.")
+
 type vertex = Ident.t
-module Edge = Set.Make(struct type t = vertex let compare = compare end)
-type edge = Edge.t
+
+type 'a formula =
+  | And of 'a formula list
+  | Var of 'a
+  | Or of 'a formula list
+  | True
+  | False
+
+let const s ppf = Format.fprintf ppf "%(%)" s
+let sep s ppf () = const s ppf
+
+let rec pp_formula var ppf = function
+  | And l -> Format.fprintf ppf "@[[%a]@]"
+               (pp_list var @@ format_of_string "∧") l
+  | Or l -> Format.fprintf ppf "@[[%a]@]" (pp_list var "∨") l
+  | True -> const "true" ppf
+  | False -> const "false" ppf
+  | Var x -> var ppf x
+and pp_list var s =
+  Format.pp_print_list ~pp_sep:(sep @@ "@ "^^ s ^^"@ ") (pp_formula var)
+let pp_ident ppf id = Format.fprintf ppf "%s" (Ident.name id)
+let pp_edge = pp_formula pp_ident
+
+
+let rec simplify assign = function
+  | True | False as x -> x
+  | And [] | Or [] -> True
+  | And [x] -> x
+  | Or [x] -> x
+  | And l -> simplify_and assign [] l
+  | Or l -> simplify_or assign [] l
+  | Var x as v ->
+    match assign x with
+    | None -> v
+    | Some x -> x
+and simplify_list max absorbing best f rest  = function
+  | [] -> if best then max else And rest
+  | x :: q ->
+    let x = simplify f x in
+    if x = absorbing then absorbing
+    else if x = max then simplify_list max absorbing true f (x::rest) q
+    else simplify_list max absorbing false f (x::rest) q
+and simplify_and f = simplify_list True False true f
+and simplify_or f = simplify_list False True true f
+
+let simplify f x =
+  let y = simplify f x in
+  debug "[%a] => [%a]" pp_edge x pp_edge y;
+  y
+
+let simplif = simplify (fun _ -> None)
+let ( %=% ) value lit = simplify (fun x -> if x = value then Some lit else None)
+
+
+type edge = Ident.t formula
 type status =
   | Unknown
   | Connected
@@ -21,7 +77,6 @@ let add_edge_simple v edge tbl =
       { info with status = max Connected info.status;
                   edges =  edge :: info.edges }
 
-
 let rec remove_vertices vs tbl =
   match vs with
   | [] -> ()
@@ -29,26 +84,27 @@ let rec remove_vertices vs tbl =
     Hashtbl.remove tbl v;
     let stack = ref q in
     Hashtbl.filter_map_inplace (fun _w info ->
-        let edges = List.map (Edge.remove v) info.edges in
+        let edges = List.map (v %=% True) info.edges in
         if info.status = Connected
-        && List.exists (fun e -> e = Edge.empty) edges then
+        && List.exists ( (=) True ) edges then
           (stack := v :: !stack; None)
         else Some { info with edges }
       ) tbl;
     remove_vertices !stack tbl
 
 let add_edge tbl vertex edge =
-  if edge = Edge.empty then remove_vertices [vertex] tbl
+  debug "Add edge:[%a]" pp_edge edge;
+  if edge = True then remove_vertices [vertex] tbl
   else add_edge_simple vertex edge tbl
 
 let add_arrow (res,ls) graph =
   let rec filter edge = function
-    | [] ->  add_edge graph res edge
+    | [] ->  add_edge graph res (simplif @@ And edge)
     | arg :: q ->
-      let edge = if Hashtbl.mem graph arg then Edge.add arg edge else edge in
+      let edge = if Hashtbl.mem graph arg then Var arg::edge else edge in
       filter edge q in
   if Hashtbl.mem graph res then
-    filter Edge.empty ls
+    filter [] ls
   else ()
 
 type view = { graph:t; mutable vertices: (Location.t * vertex) list }
